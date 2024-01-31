83e16743d32eec161a038c3b781b044d58f59dbd
bsj039190 <bsj039190@gmail.com>
2024-02-01 03:17:07 +0900

    240131 desktop commit

--- src/main/java/com/example/applejun/dto/ProfileImageDto.java
+++ src/main/java/com/example/applejun/dto/ProfileImageDto.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProfileImageDto {
+
+    private Long id;
     private String originalFileName;
     private String savedFileName;
 }

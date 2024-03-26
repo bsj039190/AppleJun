f37d62b0bc2a9662fb702c42d92b0e0e1d2e70ba
bsj039190 <bsj039190@gmail.com>
2024-03-26 19:34:39 +0900

    240326 labtop commit

--- src/frontend/applejun/src/map/MapCreate.js
+++ src/frontend/applejun/src/map/MapCreate.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import "../css/app/Background.css";
+import "../css/app/UpperbarProfile.css";
+import "../css/app/MapCreate.css";
 import {
   Container as MapDiv,
   NaverMap,
@@ -24,21 +27,57 @@ function MapCreate() {
     gpsLng: 0,
   });
   const navermaps = useNavermaps();
+  const [marker, setMarker] = useState({});
   const history = useHistory();
   const [currentUser, setCurrentUser] = useState(0);
+  const naverLink = `https://map.naver.com/p/search/${gpsRequest.name}`;
+  const [mapInstance, setMapInstance] = useState(null);
 
-  useEffect(() => {
-    const loadStoredUser = async () => {
-      const storedUser = localStorage.getItem("currentUser");
-      if (storedUser) {
-        const userObject = JSON.parse(storedUser);
-        setCurrentUser(userObject.id);
-        console.log(userObject.id);
-      }
-    };
+  const [left, setLeft] = useState({
+    name: "",
+    profileImage: "",
+  });
 
-    loadStoredUser();
-  }, []);
+  const [right, setRight] = useState({
+    name: "",
+    profileImage: "",
+  });
+
+  const extractProfileImageName = (filePath) => {
+    // filePath를 backslash(\) 또는 forward slash(/)를 기준으로 나눕니다.
+    const parts = filePath.split(/[\\/]/);
+
+    // 배열의 마지막 요소를 제거하고 반환합니다. 이것이 파일 이름입니다.
+    return parts.pop();
+  };
+
+  const fetchProfile = async (e) => {
+    const joon = await axios.get(`http://localhost:8080/account/get/1`, {
+      withCredentials: true,
+    });
+
+    const joonData = joon.data.contents;
+    if (joonData !== null) {
+      console.log(joonData);
+      setLeft({
+        name: joonData.name,
+        profileImage: extractProfileImageName(joonData.profileImage),
+      });
+    }
+
+    const soo = await axios.get(`http://localhost:8080/account/get/2`, {
+      withCredentials: true,
+    });
+
+    const sooData = soo.data.contents;
+    if (sooData !== null) {
+      console.log(sooData);
+      setRight({
+        name: sooData.name,
+        profileImage: extractProfileImageName(sooData.profileImage),
+      });
+    }
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -78,6 +117,18 @@ function MapCreate() {
           gpsLat: items[0].point.y,
           gpsLng: items[0].point.x,
         }));
+
+        const position = new navermaps.LatLng(
+          items[0].point.y,
+          items[0].point.x
+        );
+
+        const marker = new navermaps.Marker({
+          map: mapInstance,
+          position,
+        });
+
+        setMarker(marker);
       }
     );
   };
@@ -104,74 +155,189 @@ function MapCreate() {
     }
   };
 
+  useEffect(() => {
+    const loadStoredUser = async () => {
+      const storedUser = localStorage.getItem("currentUser");
+      if (storedUser) {
+        const userObject = JSON.parse(storedUser);
+        setCurrentUser(userObject.id);
+        console.log(userObject.id);
+      }
+    };
+
+    loadStoredUser();
+    fetchProfile();
+  }, []);
+
+  useEffect(() => {
+    if (gps.gpsLat == 0) {
+      // map 변수 선언
+      const mapInstance = new navermaps.Map("map", {
+        center: new navermaps.LatLng(37.5121391, 126.8426069),
+        zoom: 10,
+      });
+    } else {
+      const mapInstance = new navermaps.Map("map", {
+        center: new navermaps.LatLng(gps.gpsLat, gps.gpsLng),
+        zoom: 19,
+      });
+      console.log(mapInstance);
+      setMapInstance(mapInstance);
+
+      marker.setMap(mapInstance);
+    }
+  }, [gps]);
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={gpsRequest.name}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <label>
-          Subject:
-          <input
-            type="text"
-            name="subject"
-            value={gpsRequest.subject}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <label>
-          Address:
-          <input
-            type="text"
-            name="address"
-            value={gpsRequest.address}
-            onChange={handleChange}
-          />
-          <button onClick={(e) => onClickAddress(e, gpsRequest.address)}>
-            GPS 변환
-          </button>
-        </label>
-        <br />
-
-        <label>
-          위도: {gps.gpsLat}, 경도: {gps.gpsLng}
-        </label>
-        <br />
-
-        <label>
-          Url:
-          <input
-            type="text"
-            name="url"
-            value={gpsRequest.url}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-
-        <button type="submit">위치 추가</button>
-        {currentUser === 3 && <p>게스트 계정은 업로드 할 수 없습니다.</p>}
-      </form>
-      <br />
-      <a href="https://map.naver.com/p" target="_blank">
-        네이버 지도 링크
-      </a>
-      <br />
-      <br />
-      <div>
-        <button onClick={() => history.push(`/home`)}>홈으로</button>
+    <>
+      <div
+        id="n_1920__10"
+        className="gradient-background"
+        style={{ zIndex: -2 }}
+      >
+        <svg className="n_27_t">
+          <linearGradient
+            id="n_27_t"
+            spreadMethod="pad"
+            x1="0"
+            x2="1"
+            y1="0.5"
+            y2="0.5"
+          >
+            <stop offset="0" stopColor="#ffe0e7" stopOpacity="1"></stop>
+            <stop offset="1" stopColor="#d6eaff" stopOpacity="1"></stop>
+          </linearGradient>
+          <rect
+            id="n_27_t"
+            rx="0"
+            ry="0"
+            x="0"
+            y="0"
+            width="100%"
+            height="100%"
+          ></rect>
+        </svg>
       </div>
-    </div>
+
+      <div className="upperbarLeftSubject">
+        <img
+          src="/logos/Map.png"
+          style={{ maxWidth: "120px", maxHeight: "120px" }}
+        />
+        <p>새로운 장소</p>
+      </div>
+
+      <div className="upperbarProfileGruop">
+        <p>{left.name}</p>
+        <img
+          src={`/profile-image/${left.profileImage}`}
+          alt="leftProfile"
+          className="upperbarProfileGruopImg"
+        />
+
+        <img src="/logos/Heart.png" alt="Heart" />
+
+        <img
+          src={`/profile-image/${right.profileImage}`}
+          alt="rightProfile"
+          className="upperbarProfileGruopImg"
+        />
+        <p>{right.name}</p>
+      </div>
+
+      <div className="upperbarHomeButton">
+        <img
+          src="/logos/HomeButton.png"
+          onClick={() => history.push(`/home`)}
+        />
+      </div>
+
+      <div className="mapGroup">
+        <form onSubmit={handleSubmit}>
+          <h2>새로운 장소 저장하기</h2>
+          <div className="mapGroupLabel">
+            <label>
+              이름
+              <input
+                type="text"
+                name="name"
+                value={gpsRequest.name}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+
+          <div className="mapGroupLabel">
+            <label>
+              주소
+              <input
+                type="text"
+                name="address"
+                value={gpsRequest.address}
+                onChange={handleChange}
+              />
+              <button onClick={(e) => onClickAddress(e, gpsRequest.address)}>
+                GPS 변환
+              </button>
+            </label>
+          </div>
+
+          <div className="mapGroupLabel">
+            <label>
+              위도: {gps.gpsLat} 경도: {gps.gpsLng}
+            </label>
+          </div>
+
+          <div className="mapGroupLabel">
+            <label>
+              카테고리
+              <input
+                type="text"
+                name="subject"
+                value={gpsRequest.subject}
+                onChange={handleChange}
+              />
+            </label>
+          </div>
+
+          <div className="mapGroupLabel">
+            <label>
+              URL
+              <input
+                type="text"
+                name="url"
+                value={gpsRequest.url}
+                onChange={handleChange}
+              />
+            </label>
+            <button>
+              <a
+                href={naverLink}
+                target="_blank"
+                style={{ color: "white", textDecoration: "none" }}
+              >
+                URL 수정
+              </a>
+            </button>
+          </div>
+
+          <div className="mapGroupLabel">
+            <button type="submit">위치 추가</button>
+            {currentUser === 3 && <p>게스트 계정은 업로드 할 수 없습니다.</p>}
+          </div>
+        </form>
+
+        <div
+          id="map"
+          style={{
+            width: "500px",
+            height: "500px",
+            margin: "20px",
+            marginTop: "40px",
+          }}
+        />
+      </div>
+    </>
   );
 }
 

338dfc40f5ec01b1c3eafcd5b698a526dae43810
bsj039190 <bsj039190@gmail.com>
2024-03-30 18:35:08 +0900

    240330 labtop commit

--- src/frontend/applejun/src/post/PostWrite.js
+++ src/frontend/applejun/src/post/PostWrite.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useHistory } from "react-router-dom";
+import "../css/app/UpperbarProfile.css";
+import "../css/app/Background.css";
+import "../css/app/PostWrite.css";
 
 const PostWrite = () => {
   const [postRequest, setPostRequest] = useState({
@@ -20,28 +23,19 @@ const PostWrite = () => {
   const history = useHistory();
   const [currentUser, setCurrentUser] = useState(0);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setPostRequest({
-      ...postRequest,
-      [name]: value,
-    });
-  };
+  const [left, setLeft] = useState({
+    name: "",
+    profileImage: "",
+  });
 
-  const handleFileChange = (e) => {
-    const files = e.target.files;
-    setFileList(files);
+  const [right, setRight] = useState({
+    name: "",
+    profileImage: "",
+  });
 
-    // 이미지 미리보기 생성
-    const previews = Array.from(files).map((file) => URL.createObjectURL(file));
-    setImagePreviews(previews);
-  };
-
-  const handleDateChange = (date) => {
-    setPostRequest({
-      ...postRequest,
-      date,
-    });
+  const extractFileNameAddPath = (filePath) => {
+    const parts = filePath.split(/[\\/]/);
+    return parts.pop();
   };
 
   const getGps = async () => {
@@ -56,19 +50,58 @@ const PostWrite = () => {
     }
   };
 
-  useEffect(() => {
-    const loadStoredUser = async () => {
-      const storedUser = localStorage.getItem("currentUser");
-      if (storedUser) {
-        const userObject = JSON.parse(storedUser);
-        setCurrentUser(userObject.id);
-        console.log(userObject.id);
-      }
-    };
+  const fetchProfile = async (e) => {
+    const joon = await axios.get(`http://localhost:8080/account/get/1`, {
+      withCredentials: true,
+    });
 
-    loadStoredUser();
-    getGps();
-  }, []);
+    const joonData = joon.data.contents;
+    if (joonData !== null) {
+      console.log(joonData);
+      setLeft({
+        name: joonData.name,
+        profileImage: extractFileNameAddPath(joonData.profileImage),
+      });
+    }
+
+    const soo = await axios.get(`http://localhost:8080/account/get/2`, {
+      withCredentials: true,
+    });
+
+    const sooData = soo.data.contents;
+    if (sooData !== null) {
+      console.log(sooData);
+      setRight({
+        name: sooData.name,
+        profileImage: extractFileNameAddPath(sooData.profileImage),
+      });
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPostRequest({
+      ...postRequest,
+      [name]: value,
+    });
+  };
+
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+    setFileList(files);
+
+    // 이미지 미리보기 생성
+    const previews = Array.from(files).map((file) => URL.createObjectURL(file));
+    console.log(previews);
+    setImagePreviews(previews);
+  };
+
+  const handleDateChange = (date) => {
+    setPostRequest({
+      ...postRequest,
+      date,
+    });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -77,6 +110,11 @@ const PostWrite = () => {
       const formData = new FormData();
 
       console.log(postRequest);
+
+      if (fileList.length > 3) {
+        alert("사진은 최대 3장까지만 가능합니다.");
+        return;
+      }
 
       if (fileList.length > 0) {
         for (let i = 0; i < fileList.length; i++) {
@@ -111,132 +149,316 @@ const PostWrite = () => {
     }
   };
 
+  useEffect(() => {
+    const loadStoredUser = async () => {
+      const storedUser = localStorage.getItem("currentUser");
+      if (storedUser) {
+        const userObject = JSON.parse(storedUser);
+        setCurrentUser(userObject.id);
+        setPostRequest({ uploader: userObject.id });
+      }
+    };
+
+    loadStoredUser();
+    getGps();
+    fetchProfile();
+  }, []);
+
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Title:
-        <input
-          type="text"
-          name="title"
-          value={postRequest.title}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <label>
-        Uploader:
-        <input
-          type="text"
-          name="uploader"
-          value={currentUser}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      {/* <label>
-        Date:
-        <input
-          type="text"
-          name="date"
-          value={postRequest.date}
-          onChange={handleChange}
-        />
-      </label>
-      <br /> */}
-
-      <label>
-        Date:
-        <DatePicker
-          selected={postRequest.date}
-          onChange={handleDateChange}
-          dateFormat="yyyy-MM-dd"
-        />
-      </label>
-      <br />
-
-      <label>
-        Content:
-        <input
-          type="text"
-          name="content"
-          value={postRequest.content}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <label>
-        GPS1:
-        <select name="gps1" value={postRequest.gps1} onChange={handleChange}>
-          {gpsList.map((gps) => (
-            <option key={gps.id} value={gps.id}>
-              {gps.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <br />
-
-      <label>
-        GPS2:
-        <select name="gps2" value={postRequest.gps2} onChange={handleChange}>
-          {gpsList.map((gps) => (
-            <option key={gps.id} value={gps.id}>
-              {gps.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <br />
-
-      <label>
-        GPS3:
-        <select name="gps3" value={postRequest.gps3} onChange={handleChange}>
-          {gpsList.map((gps) => (
-            <option key={gps.id} value={gps.id}>
-              {gps.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <br />
-
-      <label>
-        File(s):
-        <input
-          type="file"
-          name="fileList"
-          multiple
-          onChange={handleFileChange}
-        />
-      </label>
-      <br />
-
-      <div>
-        {/* 이미지 미리보기 */}
-        {imagePreviews.length > 0 ? (
-          imagePreviews.map((preview, index) => (
-            <img
-              key={index}
-              src={preview}
-              alt={`Preview ${index + 1}`}
-              style={{
-                maxWidth: "300px",
-                maxHeight: "300px",
-                marginRight: "10px",
-              }}
-            />
-          ))
-        ) : (
-          <p>이미지 미리보기</p>
-        )}
+    <div>
+      <div
+        id="n_1920__10"
+        className="gradient-background"
+        style={{ zIndex: -2 }}
+      >
+        <svg className="n_27_t">
+          <linearGradient
+            id="n_27_t"
+            spreadMethod="pad"
+            x1="0"
+            x2="1"
+            y1="0.5"
+            y2="0.5"
+          >
+            <stop offset="0" stopColor="#ffe0e7" stopOpacity="1"></stop>
+            <stop offset="1" stopColor="#d6eaff" stopOpacity="1"></stop>
+          </linearGradient>
+          <rect
+            id="n_27_t"
+            rx="0"
+            ry="0"
+            x="0"
+            y="0"
+            width="100%"
+            height="100%"
+          ></rect>
+        </svg>
       </div>
 
-      <button type="submit">Submit</button>
-      {currentUser === 3 && <p>게스트 계정은 업로드 할 수 없습니다.</p>}
-    </form>
+      <div className="upperbarLeftSubject">
+        <img
+          src="/logos/StoryWrite.png"
+          style={{ width: "40px", height: "auto" }}
+        />
+        <p
+          className="customTextColorAndShadow"
+          style={{ display: "flex", marginLeft: "10px" }}
+        >
+          스토리 작성
+        </p>
+      </div>
+
+      <div className="upperbarProfileGruop customTextColorAndShadow">
+        <p>{left.name}</p>
+        <img
+          src={`/profile-image/${left.profileImage}`}
+          alt="leftProfile"
+          className="upperbarProfileGruopImg"
+        />
+
+        <a href="/home">
+          <img
+            src="/logos/Heart.png"
+            alt="Heart"
+            style={{ width: "50px", height: "50px" }}
+          />
+        </a>
+
+        <img
+          src={`/profile-image/${right.profileImage}`}
+          alt="rightProfile"
+          className="upperbarProfileGruopImg"
+        />
+        <p>{right.name}</p>
+      </div>
+
+      {/* <div className="postListHomeButton">
+        <img
+          src="/logos/HomeButton.png"
+          onClick={() => history.push(`/home`)}
+          style={{ width: "36px", height: "36px" }}
+        />
+      </div> */}
+
+      <div className="postWriteBigWrapper">
+        <p
+          style={{
+            fontSize: "30px",
+            marginLeft: "50px",
+          }}
+        >
+          <a
+            href="/post/get/list"
+            style={{
+              fontWeight: "bold",
+              color: "black",
+              textDecoration: "none",
+            }}
+          >
+            &lt;
+          </a>
+          글 작성하기
+        </p>
+
+        <div className="postWriteBoder">
+          <form
+            onSubmit={handleSubmit}
+            style={{ width: "800px", height: "70vh" }}
+          >
+            <label className="postWriteTitle">
+              {/* Title: */}
+              <input
+                type="text"
+                name="title"
+                value={postRequest.title}
+                onChange={handleChange}
+                placeholder="제목을 적어주세요."
+              />
+            </label>
+            <br />
+
+            <label className="postWriteContent">
+              {/* Content: */}
+              <input
+                type="text"
+                name="content"
+                value={postRequest.content}
+                onChange={handleChange}
+                placeholder="내용을 적어주세요."
+              />
+            </label>
+            <br />
+
+            {/* <label>
+              Uploader:
+              <input
+                type="text"
+                name="uploader"
+                value={currentUser}
+                onChange={handleChange}
+              />
+            </label> */}
+
+            {/* <label>
+              Date:
+              <input
+                type="text"
+                name="date"
+                value={postRequest.date}
+                onChange={handleChange}
+              />
+            </label>
+            <br /> */}
+
+            <div className="postWriteGpsContainer">
+              <label style={{ display: "flex", alignItems: "center" }}>
+                <img src="/logos/Mapblue.png" className="postWriteGpsLogo" />
+                <select
+                  name="gps1"
+                  value={postRequest.gps1}
+                  onChange={handleChange}
+                  style={{ marginRight: "10px" }}
+                >
+                  {gpsList.map((gps) => (
+                    <option key={gps.id} value={gps.id}>
+                      {gps.name}
+                    </option>
+                  ))}
+                </select>
+              </label>
+
+              <label style={{ display: "flex", alignItems: "center" }}>
+                <img src="/logos/Mapblue.png" className="postWriteGpsLogo" />
+                <select
+                  name="gps2"
+                  value={postRequest.gps2}
+                  onChange={handleChange}
+                  style={{ marginRight: "10px" }}
+                >
+                  {gpsList.map((gps) => (
+                    <option key={gps.id} value={gps.id}>
+                      {gps.name}
+                    </option>
+                  ))}
+                </select>
+              </label>
+
+              <label style={{ display: "flex", alignItems: "center" }}>
+                <img src="/logos/Mapblue.png" className="postWriteGpsLogo" />
+                <select
+                  name="gps3"
+                  value={postRequest.gps3}
+                  onChange={handleChange}
+                >
+                  {gpsList.map((gps) => (
+                    <option key={gps.id} value={gps.id}>
+                      {gps.name}
+                      {/*아니 기본이 계속 호안끼엠으로 되어있음 더미가 아니라*/}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </div>
+
+            <div className="postWriteSummitButton">
+              <div className="postWriteFileDateContainer">
+                <label
+                  className="postWriteCustomFile"
+                  style={{ marginRight: "20px" }}
+                >
+                  <button
+                    onClick={() =>
+                      document.querySelector('input[type="file"]').click()
+                    }
+                  >
+                    사진 업로드
+                    <input
+                      type="file"
+                      name="fileList"
+                      multiple
+                      onChange={handleFileChange}
+                    />
+                  </button>
+                </label>
+
+                <label>작성자: {postRequest.uploader}</label>
+
+                <label>
+                  날짜:
+                  <DatePicker
+                    selected={postRequest.date}
+                    onChange={handleDateChange}
+                    dateFormat="yyyy-MM-dd"
+                  />
+                </label>
+              </div>
+              <button type="submit">저장하기</button>
+            </div>
+
+            {/* <div className="postWriteImagePreview">
+              
+
+              {imagePreviews.length > 0 ? (
+                imagePreviews.map((preview, index) => (
+                  <img key={index} src={preview} alt={`Preview ${index + 1}`} />
+                ))
+              ) : (
+                <div>
+                  <img src="/logos/PostNoneBox.png" />
+                  <img src="/logos/PostNoneBox.png" />
+                  <img src="/logos/PostNoneBox.png" />
+                </div>
+              )}
+            </div> */}
+
+            <div className="postWriteImagePreview">
+              {/* 이미지 미리보기 */}
+
+              <p>엄준식</p>
+              {(() => {
+                const images = [];
+                if (imagePreviews == null) {
+                  for (let i = 0; i < 3; i++) {
+                    if (imagePreviews[i] !== null) {
+                      console.log(imagePreviews[i]);
+                      images.push(
+                        <img
+                          key={i}
+                          src={imagePreviews[i]}
+                          alt={`Preview ${i + 1}`}
+                        />
+                      );
+                    } else {
+                      images.push(
+                        <img
+                          key={i}
+                          src="/logos/PostNoneBox.png"
+                          alt={`Empty Preview ${i + 1}`}
+                        />
+                      );
+                    }
+                  }
+                } else {
+                  console.log("왜?");
+                  for (let i = 0; i < 3; i++) {
+                    images.push(
+                      <img
+                        key={i}
+                        src="/logos/PostNoneBox.png"
+                        alt={`Empty Preview ${i + 1}`}
+                      />
+                    );
+                  }
+                }
+                return images;
+              })()}
+            </div>
+
+            {currentUser === 3 && <p>게스트 계정은 업로드 할 수 없습니다.</p>}
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
